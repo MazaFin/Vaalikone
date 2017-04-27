a11a2fe9158dd66eb2aefd2239d9d6b70fdb73c0
Sami Jussinniemi <sami1531@student.hamk.fi>
2017-04-27 09:28:37 +0300

    kysymysten haku

--- src/java/vaalikone/HaeKysymykset.java
+++ src/java/vaalikone/HaeKysymykset.java
@@ -6,10 +6,18 @@ package vaalikone;
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Query;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import persist.Kysymykset;
 
 /**
  *
@@ -27,14 +35,41 @@ public class HaeKysymykset extends HttpServlet {
      * @throws ServletException if a servlet-specific error occurs
      * @throws IOException if an I/O error occurs
      */
+    private final static Logger logger = Logger.getLogger(Loki.class.getName());
+
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         response.setContentType("text/html;charset=UTF-8");
         PrintWriter out = response.getWriter();
-        try {
-            
 
-        } finally {            
+
+
+        // hae http-sessio ja luo uusi jos vanhaa ei ole vielä olemassa
+        HttpSession session = request.getSession(true);
+
+        //hae käyttäjä-olio http-sessiosta
+        Kayttaja usr = (Kayttaja) session.getAttribute("usrobj");
+
+        //jos käyttäjä-oliota ei löydy sessiosta, luodaan sinne sellainen
+        if (usr == null) {
+            usr = new Kayttaja();
+            logger.log(Level.FINE, "Luotu uusi käyttäjä-olio");
+            session.setAttribute("usrobj", usr);
+
+        }
+
+        // Hae tietokanta-yhteys contextista
+        EntityManagerFactory emf = (EntityManagerFactory) getServletContext().getAttribute("emf");
+        EntityManager em = emf.createEntityManager();
+
+        try {
+
+            Query kysely = em.createNamedQuery("Kysymykset.findAll");
+            List<Kysymykset> kysymysList = kysely.getResultList();
+            request.setAttribute("kysymykset", kysymysList);
+            request.getRequestDispatcher("Hallintapaneeli.jsp").forward(request, response);
+
+        } finally {
             out.close();
         }
     }
--- web/Hallintapaneeli.jsp
+++ web/Hallintapaneeli.jsp
@@ -5,13 +5,31 @@
 --%>
 
 <%@page contentType="text/html" pageEncoding="UTF-8"%>
+<%@page import="java.util.*,vaalikone.Vaalikone,persist.*"%>
 <!DOCTYPE html>
+
 <html>
     <head>
         <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-        <title>JSP Page</title>
+        <link href="style.css" rel="stylesheet" type="text/css">
+        <title>Hallintapaneeli</title>
     </head>
     <body>
-        <h1>Hello World!</h1>
+        <div id="container-et">
+            <div class="kysymys">
+                <form onsubmit="return confirm('Oletko varma?')" action="index.html">
+                    <%
+                        @SuppressWarnings("unchecked")
+                        List<Kysymykset> kysymykset = (List<Kysymykset>) request.getAttribute("kysymykset");
+                        for (Kysymykset kysymys : kysymykset) {%>
+
+                    <%= kysymys.getKysymysId()%>.<input class="Kinput" type="text" name="K<%= kysymys.getKysymysId()%>" value="<%= kysymys.getKysymys()%>">
+                    <br>
+                    <%            }
+                    %>
+                    <input type="submit" id="submitnappi" value="Päivitä" />
+                </form>
+            </div>
+        </div>
     </body>
 </html>
